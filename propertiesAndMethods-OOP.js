fca122780336f41d546a8fd72e9ea39d01311261
Afiq Razak <120654244+afiqrazak380@users.noreply.github.com>
2023-10-13 11:10:35 +0100

    Add files via upload

---
+++ propertiesAndMethods-OOP.js
@@ -0,0 +1,24 @@
+// Object Oriented Programming (OOP)
+// Object = a group of properties and methods
+// properties = what an object has, eg: name, colour, year
+// methods = what an object can do, eg: drive, brakes
+
+const car ={
+  name: "Proton",
+  colour: "Red",
+  year: 1999,
+
+  drive : function(){
+    console.log("You drive the car")
+  },
+  brakes : function(){
+    console.log("You press the brakes")
+  },
+}
+
+// console.log(car.name)
+// console.log(car.colour)
+// console.log(car.year)
+
+// car.drive();
+// car.brakes();
